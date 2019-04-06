f1a46d628ca06e2fb704d972cbed0b8174265a43
n-peugnet <n.peugnet@free.fr>
2019-04-07 01:16:00 +0200

    triying to get more of sklearn's potential
    - use sklearn load_data and numpy arrays
    - add back the full dataset and made it compatible with load_data
    - use sklearn pipeline
    - test some preprocessing functions
    - add some comments

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,52 +1,79 @@
 from abc import ABC, abstractmethod
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.datasets.base import load_data
+from sklearn.pipeline import make_pipeline
 from sklearn.metrics import accuracy_score
-import csv
 
 
 class Algorithm(ABC):
+    """ Abstract class representing a machine learning algorithm.
+
+    Each subclass must implement these functions:
+
+        - def configurations(self)
+    """
 
     def __init__(self):
         super().__init__()
-        self.model = None
-        self.trainingset_features = []
-        self.trainingset_labels = []
-        self.testset_features = []
-        self.testset_labels = []
+        self.train_feat = []
+        self.train_labl = []
+        self.test_feat = []
+        self.test_labl = []
         self.initSets()
 
     def initSets(self):
-        with open('./data/spambase.trainingset.csv') as csvDataFile:
-            csvReader = csv.reader(csvDataFile)
-            for row in csvReader:
-                self.trainingset_labels.append(int(row.pop()))
-                self.trainingset_features.append(list(map(lambda x: float(x), row)))
-        with open('./data/spambase.testset.csv') as csvDataFile:
-            csvReader = csv.reader(csvDataFile)
-            for row in csvReader:
-                self.testset_labels.append(int(row.pop()))
-                self.testset_features.append(list(map(lambda x: float(x), row)))
+        """Init the datasets features and labels from the data directory's csv files"""
+        features, labels, _ = load_data(".", "spambase.csv")
+        self.train_feat, self.test_feat, self.train_labl, self.test_labl = train_test_split(features, labels, test_size=0.20)
 
     def test(self):
-        return accuracy_score(self.testset_labels, self.run())
+        """Run the algorithm and return the accuracy of its predictions"""
+        return [
+            {
+                'function': func.__name__,
+                'accuracy': accuracy_score(func(), self.test_labl)
+            }
+            for func in self.configurations()]
 
-    # instanciate the model, fit it, run it then return the predictions
     @abstractmethod
-    def run(self):
+    def configurations(self):
+        """Return an array of functions returning predictions"""
         pass
 
 
 class NaiveBayes(Algorithm):
 
-    def run(self):
-        # Create a Gaussian Classifier
-        self.model = GaussianNB()
+    def configurations(self):
+        return [self.unscaled, self.scaled]
 
-        # Train the model using the training sets
-        self.model.fit(self.trainingset_features, self.trainingset_labels)
+    def unscaled(self):
+
+        # Create a Gaussian Classifier
+        model = make_pipeline(PCA(n_components=2), GaussianNB())
+
+        # Train the model using pipelined GNB and PCA.
+        model.fit(self.train_feat, self.train_labl)
 
         # Predict Output
-        predicted = self.model.predict(self.testset_features)
+        predicted = model.predict(self.test_feat)
+        print("Predicted Value:", predicted)
+
+        return predicted
+
+    def scaled(self):
+
+        # Create a Gaussian Classifier
+        model = make_pipeline(StandardScaler(), PCA(
+            n_components=2), GaussianNB())
+
+        # Train the model using pipelined scaling, GNB and PCA.
+        model.fit(self.train_feat, self.train_labl)
+
+        # Predict Output
+        predicted = model.predict(self.test_feat)
         print("Predicted Value:", predicted)
 
         return predicted
