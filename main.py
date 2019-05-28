8b16b141f33c46083a511b86f99568ebdbc9373c
n-peugnet <n.peugnet@free.fr>
2019-05-28 20:32:16 +0200

    first add of text2feature function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 Usage:
   slipspam bench [options]
-  slipspam predict [options] <file>
+  slipspam predict [options] (<email-text> | --in-text=<file> | --in-feat=<file>)
   slipspam -h | --help
   slipspam --version
 
@@ -12,9 +12,12 @@ Options:
   --version                      Show version.
   -e <nb>, --executions=<nb>     Number of executions [default: 5].
   -t <size>, --test-size=<size>  Proportion of the dataset to use for the tests [default: 0.2].
+  --in-text=<file>               Path to a file containing the text of a mail to classify.
+  --in-feat=<file>               Path to a file containing a csv of features compliant with spambase.
 """
 from src.algorithms import NaiveBayes, Svm, Knn, GradientBoosting, Mpl, RFC
 from src.benchmark import run_bench
+from src.utils import text2features
 from docopt import docopt
 
 args = docopt(__doc__, version='SlipSpam 1.0-beta.1')
@@ -35,3 +38,6 @@ test_size = float(args['--test-size'])
 
 if args['bench']:
     run_bench(algos, repetition, test_size)
+elif args['predict']:
+    print('Sorry, this function is not working yet...')
+    print(text2features(args['<email-text>']))
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,82 @@
 from numpy import newaxis
+from nltk import download
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+NLTK_DATA_DIR = 'dev/nltk_data'
 
 
 def normalize(cm):
     return cm.astype('float') / cm.sum(axis=1)[:, newaxis]
+
+
+count_words = [
+    'make',
+    'address',
+    'all',
+    '3d',
+    'our',
+    'over',
+    'remove',
+    'internet',
+    'order',
+    'mail',
+    'receive',
+    'will',
+    'people',
+    'report',
+    'addresses',
+    'free',
+    'business',
+    'email',
+    'you',
+    'credit',
+    'your',
+    'font',
+    '000',
+    'money'
+    'hp',
+    'hpl',
+    'george',
+    '650',
+    'lab',
+    'labs',
+    'telnet',
+    '857',
+    'data',
+    '415',
+    '85',
+    'technology',
+    '1999',
+    'parts',
+    'pm',
+    'direct',
+    'cs',
+    'meeting',
+    'original',
+    'project',
+    're',
+    'edu',
+    'table',
+    'conference',
+]
+
+
+def text2features(text):
+    download('punkt', NLTK_DATA_DIR)
+    # download('wordnet', NLTK_DATA_DIR)
+    tokens = word_tokenize(text)
+    total_nb_words = len(tokens)
+    # lemmatizer=WordNetLemmatizer()
+
+    counts = [0 for i in count_words]
+    features = [0 for i in range(57)]
+    for i in tokens:
+        # word = lemmatizer.lemmatize(i.lower())
+        word = i.lower()
+        if word in count_words:
+            idx = count_words.index(word)
+            counts[idx] += 1
+    for idx, nb in enumerate(counts):
+        features[idx] = nb / total_nb_words * 100
+    return features
